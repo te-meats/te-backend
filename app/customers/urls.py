b6130bf652393c715052e81bc3c42a41672a03d0
Nick <rhodesn10@gmail.com>
2024-09-04 16:11:58 -0400

    added CRUD operations for customers.

--- app/customers/urls.py
+++ app/customers/urls.py
@@ -3,7 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from customers import views
 
 urlpatterns = [
-    path('customers/', views.CustomerList.as_view())
+    path('customers/', views.CustomerList.as_view()),
+    path('customers/<int:pk>', views.CustomerList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
