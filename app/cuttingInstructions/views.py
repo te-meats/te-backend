78d2cfa198dbed72732091791846baaa47ebdf70
Nick <rhodesn10@gmail.com>
2024-11-14 11:11:33 -0500

    added additional event fields to model.

--- app/cuttingInstructions/views.py
+++ app/cuttingInstructions/views.py
@@ -1,5 +1,4 @@
 from django.http import Http404
-from django.shortcuts import render
 from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
--- app/scheduler/models.py
+++ app/scheduler/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+from customers.models import Customer
+
+
 # Create your models here.
 
 class Event(models.Model):
     title = models.CharField()
     start = models.CharField()
     end = models.CharField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    numAnimals = models.IntegerField()
